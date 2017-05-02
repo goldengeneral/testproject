bec9b19d6b5c08f1665f59e99d3ab0cc149bad47
morozov <morozov@developers-Mac-2.local>
2017-05-02 19:20:18 +0300

    0503

---
+++ APPR/OrderSummaryVC.h
@@ -0,0 +1,13 @@
+//
+//  OrderSummaryVC.h
+//  APPR
+//
+//  Created by morozov on 4/29/17.
+//  Copyright © 2017 gleb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderSummaryVC : UIViewController
+
+@end
---
+++ APPR/SideMenuVC.h
@@ -0,0 +1,13 @@
+//
+//  SideMenuVC.h
+//  APPR
+//
+//  Created by morozov on 5/2/17.
+//  Copyright © 2017 gleb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideMenuVC : UIViewController
+
+@end
